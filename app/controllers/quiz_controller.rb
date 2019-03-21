2733113698000d964a94c3cf8c38e703db95829b
Rabindranath Prithvi <rabindranath.prithvi@gmail.com>
2019-03-21 11:38:04 +0530

    Listing API - Do not repeat responded questions and show skipped questions if all questions are answered

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -4,10 +4,15 @@ class QuizController < ApplicationController
     questions = Subject.find(params[:topic_id]).questions if params[:subject_id]
     questions = Topic.find(params[:subject_id]).questions if params[:topic_id]
     questions = Chapter.find(params[:chapter_id]).questions if params[:chapter_id]
+    
+    issued_question_ids = Quiz.where.not(response: nil).pluck(:question_id) 
+    issued_questions = Question.where(id: issued_question_ids) 
+    questions = questions - issued_questions 
 
-  	@quiz = questions.map do |question|
-  		  User.first.quiz.create(question: question)
+  	@quiz = questions.shuffle.map do |question|
+  		  User.first.quiz.find_or_create_by(question: question)
     end
+    @quiz = User.first.quiz.where(is_skipped: true).shuffle if @quiz.empty?
   end
 
   def update
@@ -15,7 +20,7 @@ class QuizController < ApplicationController
     response = Answer.find(params[:response])
     is_skipped = response.option == 'Skip'
     is_correct = response.is_correct unless is_skipped
-    
+
     quiz.update(response: response.id,
                 is_correct: is_correct,
                 is_skipped: is_skipped)
--- app/views/quiz/index.json.jbuilder
+++ app/views/quiz/index.json.jbuilder
@@ -1,7 +1,7 @@
 json.quizz @quiz do|quiz|
  json.quiz_id quiz.id
  json.question quiz.question.question
-  json.options quiz.question.answers do|answer|
+  json.options quiz.question.answers.shuffle do|answer|
     json.answer_id answer.id
     json.option answer.option
   end
