74da65bcb1ac11593729b10b287f04478bccaba0
Rabindranath Prithvi <rabindranath.prithvi@gmail.com>
2019-03-26 07:36:18 +0530

    Add comment

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,8 @@
 class Quiz < ApplicationRecord
+  
   self.table_name =  'quizz'
 
+  # If user is implemented, quiz should also belong to user
   belongs_to :question
 
   scope :answered, -> { where( is_skipped: false ) }
--- db/migrate/20190320042201_create_quizzes.rb
+++ db/migrate/20190320042201_create_quizzes.rb
@@ -1,5 +1,6 @@
 class CreateQuizzes < ActiveRecord::Migration[5.0]
   def change
+  	# add user_id column if User is implemented
     create_table :quizz do |t|
       t.references :question, foreign_key: true, null: false
       t.integer :response
